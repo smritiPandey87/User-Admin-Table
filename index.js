24e7a76a6613cdbdd44a5404815d9dca7e67fbf0
smriti <rajsimmi369@gmail.com>
2024-10-09 23:28:28 +0530

    Add basic JS form validation

---
+++ index.js
@@ -0,0 +1,14 @@
+let form = document.getElementById('form');
+let fullNameInput = document.querySelector('input[placeholder="Enter Your Name"]');
+let emailInput = document.querySelector('input[placeholder="Enter Your Email"]');
+let  roleSelect = document.getElementById('role');
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+
+let table= document.querySelector('tableContainer');
+
+let users = [];
+
+
+
